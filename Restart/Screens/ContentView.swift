8ff2381521dd2a919f0aa82e3e38e6fd40df1860
pjevic <milanpjevic@icloud.com>
2023-12-02 15:40:22 +0100

    Final commit

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Milan Pjevic on 24.11.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
